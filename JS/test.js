d60facfb13e3860a300070e307cac9de4e75cbd2
cooldude72463 <kieransmollet0@gmail.com>
2018-03-13 23:42:20 +0000

    new api key

--- JS/test.js
+++ JS/test.js
@@ -1,6 +1,6 @@
 function test(){
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://www.omdbapi.com/?apikey=2f754ceb&", false);
+  xhr.open("GET", "http://www.omdbapi.com/?apikey=a40072ee&", false);
   xhr.send();
   alert(xhr.status);
   alert(xhr.statusText);
