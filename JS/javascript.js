5c49b149c90db4716d913e58ce422b77538da61f
DANIEL MERCIK (1513953) <1513953@rgu.ac.uk>
2018-03-22 10:40:48 +0000

    Motd working (placeholders)

--- JS/javascript.js
+++ JS/javascript.js
@@ -14,3 +14,10 @@ function login(){
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 }
+
+function motd(){
+  var motdArray = ["I did not hit her! I did naaaaht!","Oh hi Mark!","Hi doggie!"];
+
+  document.getElementById("Title").innerHTML = motdArray[Math.floor((Math.random() * motdArray.length-1) + 1)];
+  document.getElementById("subheader").innerHTML = motdArray[Math.floor((Math.random() * motdArray.length-1) + 1)];
+}
