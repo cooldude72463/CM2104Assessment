b45f6f73d69525f19b071bcfc44f5dac259fbc64
Dan1003 <daniolek12345@gmail.com>
2018-03-22 19:13:41 +0000

    small JS fix (No null console error)

--- JS/TMDBStuff.js
+++ JS/TMDBStuff.js
@@ -60,5 +60,5 @@ function motd(){
   var motdArray = [" I did not hit her, it's not true! It's bullshit! I did not hit her! [throws water bottle]","Oh hi Mark!","Hi doggie!","You betrayed me! You're not good. You, you're just a chicken. Chip-chip-chip-chip-cheep-cheep."];
 
   document.getElementById("Title").innerHTML = motdArray[Math.floor((Math.random() * motdArray.length-1) + 1)];
-  document.getElementById("subheader").innerHTML = motdArray[Math.floor((Math.random() * motdArray.length-1) + 1)];
+  //document.getElementById("subheader").innerHTML = motdArray[Math.floor((Math.random() * motdArray.length-1) + 1)];
 }
