59dde33529365e5c10de24605a80616cbaa65138
cooldude72463 <kieransmollet0@gmail.com>
2018-03-22 14:25:17 +0000

    Got the api with the searchpage working, now if we don't get the results showing on the main page, we have this.

--- JS/OMDBStuff.js
+++ JS/OMDBStuff.js
@@ -1,15 +1,34 @@
-$(function(){
-  $('#searchform').submit(function(){
-    var searchterms = $("#searchbox").val();
-    getResultsFromOMDB(searchterms);
-    return false;
-  });
-});
+function validate(){
+  var searchbox = document.getElementById("searchbox");
+	var searchvalue = searchbox.value;
+
+	if(searchvalue != ""){
+				return true;
+			} else {
+				alert("please type a search query!");
+				return false;
+			}
+}
+
+function yes(){
+  var searchString = document.location.search;
+  searchString = searchString.substring(1);
+  var nvPairs = searchString.split("&");
+
+	for (i = 0; i < nvPairs.length; i++) {
+	   var nvPair = nvPairs[i].split("=");
+	   var name = nvPair[0];
+	   var value = nvPair[1];
+  }
+  console.log(value);
+	getResultsFromOMDB(value);
+  return false;
+}
 
 function getResultsFromOMDB(searchterms){
   var url = "http://www.omdbapi.com/?apikey=a40072ee&s=" + searchterms;
   $.getJSON(url, function(jsondata){
-    addResultTitles(jsondata);
+  addResultTitles(jsondata);
   });
 }
 
@@ -21,5 +40,5 @@ function addResultTitles(jsondata){
     var year = jsondata.Search[i].Year;
     htmlstring+= "<li>" + title + ", " + year + "</li>";
   }
-  $("#mostPopular").html(htmlstring);
+  $("#searchResults").html(htmlstring);
 }
