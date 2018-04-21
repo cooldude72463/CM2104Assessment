b0a0dae05063cd87a3b978d55f5663c0d1fd7fd3
cooldude72463 <kieransmollet0@gmail.com>
2018-04-21 22:42:24 +0100

    changed the server.js

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get('/SearchPage', function(req, res) {
  res.render('pages/SearchPage');
 });
 
+app.get('/MoviePage', function(req, res) {
+ res.render('pages/MoviePage');
+});
+
+
 app.use(express.static(__dirname + '/views'));
 
 app.listen(8080);
