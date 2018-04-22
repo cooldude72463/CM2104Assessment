2ec8bdf41dbb04646b1a1433346692f12a9c84c1
cooldude72463 <kieransmollet0@gmail.com>
2018-04-22 16:54:40 +0100

    trying

--- JS/Login.js
+++ JS/Login.js
@@ -1,9 +1,25 @@
 function Login(){
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://localhost:27017/MovieReviews";
+
   var firstname = document.getElementById("firstname");
   var surname = document.getElementById("surname");
   var username = document.getElementById("username");
   var password = document.getElementById("password");
 
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.collection('UserInfo').save(userdata, function(err, result) {
+      if (err) throw err;
+      console.log('Saved')
+      res.redirect('/')
+      db.close();
+    });
+  });
+}
+/*
  db.collection('UserInfo').find().toArray(function(err, result) {
  if (err) throw err;
  var output = "<h1>Info</h1>";
@@ -22,3 +38,4 @@ function Login(){
   })
 })
 }
+*/
