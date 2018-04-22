b29cd4622b27d598a179b60ea73af9076b1f94cc
cooldude72463 <kieransmollet0@gmail.com>
2018-04-22 04:10:43 +0100

    why not

--- JS/TMDBAPI.js
+++ JS/TMDBAPI.js
@@ -45,7 +45,7 @@ function addResultTitles(jsondata){
   var counter = 0;
 
   for(var i = 0; i < jsondata.results.length; i++){
-    console.log(jsondata.results[i].title);
+    console.log(jsondata.results[i].name);
     if(jsondata.results[i].title == null){
       console.log(jsondata.results[i].vote_average);
     } else {
